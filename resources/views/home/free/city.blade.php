ae2ee8b006e6c5403203d7499e676138031fa4b2
RZZLUGLXHNDRCQU\Administrator <zm_happiness66@126.com>
2018-03-30 10:14:39 +0800

    by Amy

--- resources/views/home/free/city.blade.php
+++ resources/views/home/free/city.blade.php
@@ -5,38 +5,63 @@
     <ul class="style-padding-20">
         <li class="style-ul-left">
             <img src="{{URL::asset('img/example.jpg')}}" class="style-ul-image" />
-            <div class="style-ul-title">
-                意大利简介
+            <div class="style-width-100 style-display-table">
+                <div class="style-ul-title style-vertical-align-middle style-display-table-cell">
+                    <div class="aui-ellipsis-2">
+                        意大利简介
+                    </div>
+                </div>
             </div>
         </li>
         <li class="style-ul-right">
             <img src="{{URL::asset('img/example.jpg')}}" class="style-ul-image" />
-            <div class="style-ul-title">
-                意大利简介
+            <div class="style-width-100 style-display-table">
+                <div class="style-ul-title style-vertical-align-middle style-display-table-cell">
+                    <div class="aui-ellipsis-2">
+                        意大利简介
+                    </div>
+                </div>
             </div>
         </li>
         <li class="style-ul-left">
             <img src="{{URL::asset('img/example.jpg')}}" class="style-ul-image" />
-            <div class="style-ul-title">
-                意大利简介
+
+            <div class="style-width-100 style-display-table">
+                <div class="style-ul-title style-vertical-align-middle style-display-table-cell">
+                    <div class="aui-ellipsis-2">
+                        意大利简介
+                    </div>
+                </div>
             </div>
         </li>
         <li class="style-ul-right">
             <img src="{{URL::asset('img/example.jpg')}}" class="style-ul-image" />
-            <div class="style-ul-title">
-                意大利简介
+            <div class="style-width-100 style-display-table">
+                <div class="style-ul-title style-vertical-align-middle style-display-table-cell">
+                    <div class="aui-ellipsis-2">
+                        意大利简介
+                    </div>
+                </div>
             </div>
         </li>
         <li class="style-ul-left">
             <img src="{{URL::asset('img/example.jpg')}}" class="style-ul-image" />
-            <div class="style-ul-title">
-                意大利简介
+            <div class="style-width-100 style-display-table">
+                <div class="style-ul-title style-vertical-align-middle style-display-table-cell">
+                    <div class="aui-ellipsis-2">
+                        意大利简介
+                    </div>
+                </div>
             </div>
         </li>
         <li class="style-ul-right">
             <img src="{{URL::asset('img/example.jpg')}}" class="style-ul-image" />
-            <div class="style-ul-title">
-                意大利简介
+            <div class="style-width-100 style-display-table">
+                <div class="style-ul-title style-vertical-align-middle style-display-table-cell">
+                    <div class="aui-ellipsis-2">
+                        意大利简介
+                    </div>
+                </div>
             </div>
         </li>
     </ul>
