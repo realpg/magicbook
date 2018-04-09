8f7dd5c565281878c15f217616a04f2fb6e97f2a
RZZLUGLXHNDRCQU\Administrator <zm_happiness66@126.com>
2018-04-09 11:55:20 +0800

    by Amy

--- app/Http/Controllers/Home/ServiceController.php
+++ app/Http/Controllers/Home/ServiceController.php
@@ -15,6 +15,7 @@ class ServiceController extends Controller{
     //服务条款
     public function index(Request $request){
         $request=$request->all();
+        $common=$request['common'];
         $menu='service';
         $service_param=array();
         $service=Utils::curl('m/agreement/',$service_param);
@@ -23,6 +24,7 @@ class ServiceController extends Controller{
         $param=array(
             'menu'=>$menu,
             'service'=>$service,
+            'common'=>$common
         );
         return view('home.service.index',$param);
     }
