de99918fb296e0661f987489bcc12f6450e02dae
TerryQi <15840345959@139.com>
2018-04-23 14:56:31 +0800

    优化手机号校验

--- resources/views/home/sign/in.blade.php
+++ resources/views/home/sign/in.blade.php
@@ -13,14 +13,17 @@
                     </div>
                 </div>
                 <div>
-                    <div class="col-xs-8 col-sm-8 padding-top-40 padding-bottom-40 padding-right-50 padding-left-50" style="border-right:1px #ccc solid;">
+                    <div class="col-xs-8 col-sm-8 padding-top-40 padding-bottom-40 padding-right-50 padding-left-50"
+                         style="border-right:1px #ccc solid;">
                         <form id="form-sign-in" method="post">
                             {{csrf_field()}}
                             <div class="form-group">
-                                <input type="text" name="mobile" id="mobile" required="required" class="form-control height-50px" placeholder="请输入手机号">
+                                <input type="text" name="mobile" id="mobile" required="required"
+                                       class="form-control height-50px" placeholder="请输入手机号">
                             </div>
                             <div class="form-group margin-top-30">
-                                <input type="password" name="password" id="password" required="required" class="form-control height-50px" placeholder="请输入密码">
+                                <input type="password" name="password" id="password" required="required"
+                                       class="form-control height-50px" placeholder="请输入密码">
                             </div>
                             <div class="margin-top-30">
                                 <div class="col-xs-6 col-sm-6 padding-0">
@@ -28,21 +31,24 @@
                                     <span class="margin-left-5">记住密码</span>
                                 </div>
                                 {{--<div class="col-xs-6 col-sm-6 font-color-red padding-0 text-right">--}}
-                                    {{--<a href="">--}}
-                                        {{--忘记密码？--}}
-                                    {{--</a>--}}
+                                {{--<a href="">--}}
+                                {{--忘记密码？--}}
+                                {{--</a>--}}
                                 {{--</div>--}}
                             </div>
                             <div class="clear"></div>
                             <div class="margin-top-30">
-                                <button type="submit" class="btn btn-danger bg-none bg-red border-color-red width-100 height-40px font-size-18 border-radius-0 height-50px">立 即 登 录</button>
+                                <button type="submit"
+                                        class="btn btn-danger bg-none bg-red border-color-red width-100 height-40px font-size-18 border-radius-0 height-50px">
+                                    立 即 登 录
+                                </button>
                             </div>
                         </form>
                     </div>
                     <div class="col-xs-4 col-sm-4 text-center padding-top-40 padding-bottom-40">
                         <div class="width-200px text-algin-center margin-auto">
                             <div class="width-200px height-200px margin-bottom-20">
-                                <img src="{{URL::asset('img/erweima_03.png')}}" class="width-100" />
+                                <img src="{{URL::asset('img/erweima_03.png')}}" class="width-100"/>
                             </div>
                             <h4>扫描加魔法路书公众号</h4>
                         </div>
@@ -54,20 +60,20 @@
 @endsection
 @section('script')
     <script>
-        $(function(){
+        $(function () {
             var oForm = $('#form-sign-in');
             var oMbl = $('#mobile');
             var oPswd = $('#password');
             var oRemember = $('#remember');
             //页面初始化时，如果帐号密码cookie存在则填充
-            if(getCookie('mbl') && getCookie('pswd')){
+            if (getCookie('mbl') && getCookie('pswd')) {
                 oMbl.val(getCookie('mbl'))
                 oPswd.val(getCookie('pswd'))
-                oRemember.attr('checked','checked')
+                oRemember.attr('checked', 'checked')
             }
             //复选框勾选状态发生改变时，如果未勾选则清除cookie
-            oRemember.onchange = function(){
-                if(!this.checked){
+            oRemember.onchange = function () {
+                if (!this.checked) {
                     delCookie('mbl');
                     delCookie('pswd');
                 }
@@ -93,7 +99,7 @@
                                 message: '手机号不能为空'
                             },
                             regexp: {
-                                regexp: /^[1][3,4,5,7,8][0-9]{9}$/,
+                                regexp: /^[1][3,4,5,6,7,8][0-9]{9}$/,
                                 message: '请输入正确的手机号'
                             },
                         }
@@ -106,7 +112,7 @@
                         }
                     },
                 },
-            }).on("success.form.bv",function(e){
+            }).on("success.form.bv", function (e) {
                 // Prevent form submission
                 e.preventDefault();
 
@@ -118,56 +124,59 @@
 
                 // Use Ajax to submit form data
                 $("body").mLoading();
-                $.post("{{ URL::asset('sign/in')}}", $form.serialize(), function(ret) {
+                $.post("{{ URL::asset('sign/in')}}", $form.serialize(), function (ret) {
                     console.log('sign in ret is :' + JSON.stringify(ret))
                     $("body").mLoading("hide");
-                    if(ret.result){
+                    if (ret.result) {
                         //表单提交事件触发时，如果复选框是勾选状态则保存cookie
-                        if($('#remember').is(':checked')){
-                            setCookie('mbl',$('#mobile').val(),7); //保存帐号到cookie，有效期7天
-                            setCookie('pswd',$('#password').val(),7); //保存密码到cookie，有效期7天
-                            console.log('cookie is : '+JSON.stringify(getCookie('cs')))
-                            if(getCookie('mbl')&&getCookie('pswd')){
+                        if ($('#remember').is(':checked')) {
+                            setCookie('mbl', $('#mobile').val(), 7); //保存帐号到cookie，有效期7天
+                            setCookie('pswd', $('#password').val(), 7); //保存密码到cookie，有效期7天
+                            console.log('cookie is : ' + JSON.stringify(getCookie('cs')))
+                            if (getCookie('mbl') && getCookie('pswd')) {
                                 layer.msg('登录成功', {icon: 1, time: 3000});
                                 {{--window.location.href = "{{URL::asset('center')}}";--}}
                                     window.location.href = "{{URL::asset('index')}}";
                             }
                         }
-                        else{
+                        else {
                             layer.msg('登录成功', {icon: 1, time: 3000});
                             window.location.href = "{{URL::asset('center')}}";
                         }
                     }
-                    else{
+                    else {
                         layer.msg(ret.message, {icon: 2, time: 3000});
                     }
                 }, 'json');
             });
         })
         //设置cookie
-        function setCookie(name,value,day){
+        function setCookie(name, value, day) {
             var date = new Date();
             date.setDate(date.getDate() + day);
-            document.cookie = name + '=' + value + ';expires='+ date;
+            document.cookie = name + '=' + value + ';expires=' + date;
 
             // $.cookie(name, value);
-        };
+        }
+        ;
         //获取cookie
-        function getCookie(name){
-            var reg = RegExp(name+'=([^;]+)');
+        function getCookie(name) {
+            var reg = RegExp(name + '=([^;]+)');
             var arr = document.cookie.match(reg);
-            if(arr){
+            if (arr) {
                 return arr[1];
-            }else{
+            } else {
                 return '';
             }
 
             // $.cookie(name);
-        };
+        }
+        ;
         //删除cookie
-        function delCookie(name){
-            setCookie(name,null,-1);
+        function delCookie(name) {
+            setCookie(name, null, -1);
             // $.cookie(name, null);
-        };
+        }
+        ;
     </script>
 @endsection
--- resources/views/home/sign/up.blade.php
+++ resources/views/home/sign/up.blade.php
@@ -1,152 +1,162 @@
 @extends('home.layouts.base')
 
 @section('content')
-<div id="main-body">
-    <div class="container package-card padding-top-150 padding-bottom-150 font-color-light-grey">
-        <div class="card-div row border-radius-10px margin-left-10 margin-right-10">
-            <div class="height-50px line-height-50 bg-grey-white">
-                <div class="col-xs-8 col-sm-8">注册</div>
-                <div class="col-xs-4 col-sm-4 text-right">
-                    <a href="{{URL::asset('sign/in')}}">
-                    登录
-                    </a>
-                </div>
-            </div>
-            <div>
-                <div class="col-xs-8 col-sm-8 padding-top-40 padding-bottom-40 padding-right-50 padding-left-50" style="border-right:1px #ccc solid;">
-                    <form id="form-sign-up" method="post" >
-                        {{csrf_field()}}
-                        <div class="form-group">
-                            <input type="text" required="required" class="form-control height-50px" name="username" id="username" placeholder="请输入您的姓名">
-                        </div>
-                        <div class="form-group margin-top-30">
-                            <input type="text" required="required" class="form-control height-50px" name="mobile" id="mobile" placeholder="请输入您的手机号">
-                        </div>
-                        <div class="form-group margin-top-30">
-                            <input type="text" required="required" class="form-control height-50px" name="company" id="company" placeholder="请输入您的公司名称">
-                        </div>
-                        <div class="margin-top-30">
-                            <div class="padding-0">
-                                <div class="float-left margin-right-10">
-                                    业务类型<br />
-                                    <span class="font-size-10">（多选）</span>
-                                </div>
-                                <div class="float-left">
-                                    @if($businesses)
-                                        @foreach($businesses['results'] as $business)
-                                            <label><input type="checkbox" name="businesses[]" value="{{$business['code']}}" class="vertical-align-top"></label>
-                                            {{$business['name']}}
-                                        @endforeach
-                                    @endif
-                                </div>
-                            </div>
-                        </div>
-                        <div class="clear"></div>
-                        <div class="margin-top-30">
-                            <div class="padding-0">
-                                <input type="radio" id="female" name="agreement_checked">
-                                <label for="female" class="font-color-red">
-                                    <a href="{{URL::asset('service')}}">
-                                        我已阅读并同意《魔法路书服务条款》
-                                    </a>
-                                </label>
-                            </div>
-                        </div>
-                        <div class="clear"></div>
-                        <div class="margin-top-30">
-                            <button type="submit" id="validateBtn" class="btn btn-danger bg-none bg-red border-color-red width-100 height-40px font-size-18 border-radius-0 height-50px">立 即 注 册</button>
-                        </div>
-                    </form>
-                </div>
-                <div class="text-algin-center col-xs-4 col-sm-4 padding-top-40 padding-bottom-40 padding-right-40 padding-left-40">
-                    <div class="text-left font-size-16"><b>注册魔法路书：</b></div>
-                    <div class="margin-top-20 margin-bottom-20 padding-top-10 padding-bottom-10 font-size-16">中文语音导游，让旅行更有内涵</div>
-                    <div class="width-200px margin-auto">
-                        <img src="{{URL::asset('img/erweima_03.png')}}" class="width-100" />
+    <div id="main-body">
+        <div class="container package-card padding-top-150 padding-bottom-150 font-color-light-grey">
+            <div class="card-div row border-radius-10px margin-left-10 margin-right-10">
+                <div class="height-50px line-height-50 bg-grey-white">
+                    <div class="col-xs-8 col-sm-8">注册</div>
+                    <div class="col-xs-4 col-sm-4 text-right">
+                        <a href="{{URL::asset('sign/in')}}">
+                            登录
+                        </a>
                     </div>
-                    <div class="padding-top-10 padding-bottom-10 font-size-16">扫描加魔法路书公众号</div>
+                </div>
+                <div>
+                    <div class="col-xs-8 col-sm-8 padding-top-40 padding-bottom-40 padding-right-50 padding-left-50"
+                         style="border-right:1px #ccc solid;">
+                        <form id="form-sign-up" method="post">
+                            {{csrf_field()}}
+                            <div class="form-group">
+                                <input type="text" required="required" class="form-control height-50px" name="username"
+                                       id="username" placeholder="请输入您的姓名">
+                            </div>
+                            <div class="form-group margin-top-30">
+                                <input type="text" required="required" class="form-control height-50px" name="mobile"
+                                       id="mobile" placeholder="请输入您的手机号">
+                            </div>
+                            <div class="form-group margin-top-30">
+                                <input type="text" required="required" class="form-control height-50px" name="company"
+                                       id="company" placeholder="请输入您的公司名称">
+                            </div>
+                            <div class="margin-top-30">
+                                <div class="padding-0">
+                                    <div class="float-left margin-right-10">
+                                        业务类型<br/>
+                                        <span class="font-size-10">（多选）</span>
+                                    </div>
+                                    <div class="float-left">
+                                        @if($businesses)
+                                            @foreach($businesses['results'] as $business)
+                                                <label><input type="checkbox" name="businesses[]"
+                                                              value="{{$business['code']}}" class="vertical-align-top"></label>
+                                                {{$business['name']}}
+                                            @endforeach
+                                        @endif
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="clear"></div>
+                            <div class="margin-top-30">
+                                <div class="padding-0">
+                                    <input type="radio" id="female" name="agreement_checked">
+                                    <label for="female" class="font-color-red">
+                                        <a href="{{URL::asset('service')}}">
+                                            我已阅读并同意《魔法路书服务条款》
+                                        </a>
+                                    </label>
+                                </div>
+                            </div>
+                            <div class="clear"></div>
+                            <div class="margin-top-30">
+                                <button type="submit" id="validateBtn"
+                                        class="btn btn-danger bg-none bg-red border-color-red width-100 height-40px font-size-18 border-radius-0 height-50px">
+                                    立 即 注 册
+                                </button>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="text-algin-center col-xs-4 col-sm-4 padding-top-40 padding-bottom-40 padding-right-40 padding-left-40">
+                        <div class="text-left font-size-16"><b>注册魔法路书：</b></div>
+                        <div class="margin-top-20 margin-bottom-20 padding-top-10 padding-bottom-10 font-size-16">
+                            中文语音导游，让旅行更有内涵
+                        </div>
+                        <div class="width-200px margin-auto">
+                            <img src="{{URL::asset('img/erweima_03.png')}}" class="width-100"/>
+                        </div>
+                        <div class="padding-top-10 padding-bottom-10 font-size-16">扫描加魔法路书公众号</div>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
 @section('script')
-<script>
-    $(function() {
-        $('#form-sign-up').bootstrapValidator({
-            message: 'This value is not valid',
-            feedbackIcons: {
-                valid: 'glyphicon glyphicon-ok',
-                invalid: 'glyphicon glyphicon-remove',
-                validating: 'glyphicon glyphicon-refresh'
-            },
-            fields: {
-                username: {
-                    // message: '用户名无效',
-                    validators: {
-                        notEmpty: {
-                            message: '用户名不能为空'
-                        },
-                        stringLength: {//检测长度
-                            min: 0,
-                            max: 10,
-                            message: '长度必须在0-10之间'
-                        },
-                        regexp: {
-                            regexp: /^[\u4E00-\u9FA5a-zA-Z0-9_]*$/,
-                            message: '只能输入中文、字母、数字、下划线'
-                        },
-                    }
+    <script>
+        $(function () {
+            $('#form-sign-up').bootstrapValidator({
+                message: 'This value is not valid',
+                feedbackIcons: {
+                    valid: 'glyphicon glyphicon-ok',
+                    invalid: 'glyphicon glyphicon-remove',
+                    validating: 'glyphicon glyphicon-refresh'
                 },
-                mobile: {
-                    validators: {
-                        notEmpty: {
-                            message: '手机号不能为空'
-                        },
-                        regexp: {
-                            regexp: /^[1][3,4,5,7,8][0-9]{9}$/,
-                            message: '请输入正确的手机号'
-                        },
-                    }
-                },
-                company: {
-                    validators: {
-                        notEmpty: {
-                            message: '公司名称不能为空'
+                fields: {
+                    username: {
+                        // message: '用户名无效',
+                        validators: {
+                            notEmpty: {
+                                message: '用户名不能为空'
+                            },
+                            stringLength: {//检测长度
+                                min: 0,
+                                max: 10,
+                                message: '长度必须在0-10之间'
+                            },
+                            regexp: {
+                                regexp: /^[\u4E00-\u9FA5a-zA-Z0-9_]*$/,
+                                message: '只能输入中文、字母、数字、下划线'
+                            },
                         }
-                    }
-                },
-                agreement_checked: {
-                    validators: {
-                        notEmpty: {
-                            message: '请阅读并同意《魔法路书服务条款》'
+                    },
+                    mobile: {
+                        validators: {
+                            notEmpty: {
+                                message: '手机号不能为空'
+                            },
+                            regexp: {
+                                regexp: /^[1][3,4,5,6,7,8][0-9]{9}$/,
+                                message: '请输入正确的手机号'
+                            },
                         }
-                    }
+                    },
+                    company: {
+                        validators: {
+                            notEmpty: {
+                                message: '公司名称不能为空'
+                            }
+                        }
+                    },
+                    agreement_checked: {
+                        validators: {
+                            notEmpty: {
+                                message: '请阅读并同意《魔法路书服务条款》'
+                            }
+                        }
+                    },
                 },
-            },
-        }).on("success.form.bv",function(e){
-            // Prevent form submission
-            e.preventDefault();
+            }).on("success.form.bv", function (e) {
+                // Prevent form submission
+                e.preventDefault();
 
-            // Get the form instance
-            var $form = $(e.target);
+                // Get the form instance
+                var $form = $(e.target);
 
-            // Get the BootstrapValidator instance
-            var bv = $form.data('bootstrapValidator');
+                // Get the BootstrapValidator instance
+                var bv = $form.data('bootstrapValidator');
 
-            // Use Ajax to submit form data
-            $("body").mLoading();
-            $.post("{{ URL::asset('sign/up')}}", $form.serialize(), function(ret) {
-                $("body").mLoading("hide");
-                if(ret.result){
-                    window.location.href = "{{URL::asset('sign/success')}}";
-                }
-                else{
-                    layer.msg(ret.message, {icon: 2, time: 3000});
-                }
-            }, 'json');
-        });
-    })
-</script>
+                // Use Ajax to submit form data
+                $("body").mLoading();
+                $.post("{{ URL::asset('sign/up')}}", $form.serialize(), function (ret) {
+                    $("body").mLoading("hide");
+                    if (ret.result) {
+                        window.location.href = "{{URL::asset('sign/success')}}";
+                    }
+                    else {
+                        layer.msg(ret.message, {icon: 2, time: 3000});
+                    }
+                }, 'json');
+            });
+        })
+    </script>
 @endsection
